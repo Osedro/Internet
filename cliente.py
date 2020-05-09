260534435d248e29aff83713da1369b8f3f9bd0a
Leonardo Lopes <leonardolopes415@gmail.com>
2020-05-09 17:13:49 -0300

    Comunicação basica servidor-cliente

---
+++ cliente.py
@@ -0,0 +1,19 @@
+from socket import *
+
+# IP do servidor
+serverHost = 'DIGITE O IP DO SERVIDOR AQUI'
+serverPort = 50007
+
+#mensagem = [b'Salve salve, rapaziada do windows']
+entrada = input()
+mensagem = []
+mensagem.append(entrada.encode())
+
+objsocket = socket(AF_INET, SOCK_STREAM)
+objsocket.connect((serverHost, serverPort))
+
+for linha in mensagem:
+    objsocket.send(linha)
+
+    data = objsocket.recv(1024)
+    print('Cliente recebeu: ', data)
